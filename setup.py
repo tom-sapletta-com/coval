9dca934e8ae9bc5efad90c38ccffe849effcfb45
Tom Softreck <tom@sapletta.com>
2025-10-01 01:02:56 +0200

    Bump version to 2.0.28

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ def read_requirements():
 
 setup(
     name="coval",
-    version="2.0.27",
+    version="2.0.28",
     author="Tom Sapletta",
     author_email="info@softreck.dev",
     description="Intelligent code generation, execution, and repair system with iterative Docker deployments",
